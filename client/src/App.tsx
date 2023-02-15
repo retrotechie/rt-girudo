1cc209e45ebc8c9d2a537274c9e89add1ff9e64b
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-02-15 14:18:55 -0800

    feat(home): create `Layout` component
    `Layout` component renders `NavBar`, `LeftBar` and `RightBar`. It
    implements `Outlet` to render child route elements for `Home` content.
    
    Issue #18

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,39 @@
+import Navbar from "./components/Navbar/Navbar";
+import Leftbar from "./components/Leftbar/Leftbar";
+import Rightbar from "./components/Rightbar/Rightbar";
+
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 
 function App() {
+  // Use PascalCase for component names, got error with `layout`
+  const Layout = (): JSX.Element => {
+    // `Outlet` renders child route elements e.g. `Home`, `Profile`
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <Leftbar />
+          <Outlet />
+          <Rightbar />
+        </div>
+      </div>
+    );
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+      ],
     },
     {
       path: "/login",
