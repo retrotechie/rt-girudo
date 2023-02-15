ff190782547886fa1e42c33e0bcbd504c4b2a2ed
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-02-14 18:34:16 -0800

    feat(register): design `Register` page
    `Register` has the same design with `Login` but implemented
    `flex-direction: row-reverse;` and different `background` image.
    
    Issue #4

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,7 +1,33 @@
 import "./register.scss";
 
 const Register = () => {
-  return <div className="register">Register</div>;
+  return (
+    <div className="register">
+      <div className="card">
+        <div className="left">
+          <h1>Hi There</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias
+            magni omnis, optio dicta ipsam minima natus dignissimos, est numquam
+            animi obcaecati facilis iste tempora ex et voluptates aliquam
+            dolorum? Ratione!
+          </p>
+          <span>Do you have an account?</span>
+          <button>Login</button>
+        </div>
+        <div className="right">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
