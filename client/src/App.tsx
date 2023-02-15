0ef9fb43fd946b7d457a40a89c4c60f78050ff9c
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-02-15 13:56:37 -0800

    feat(home): create `Home` page
    Add `Home` routing to `router`.
    
    Issue #18

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
+import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
     {
       path: "/login",
       element: <Login />,
