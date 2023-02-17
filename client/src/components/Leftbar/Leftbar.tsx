3a617a2409dadb3a5fae6c3177c424a7dc09a979
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-02-16 16:19:01 -0800

    feat(leftbar): design base `Leftbar`
    Issue #20

--- client/src/components/Leftbar/Leftbar.tsx
+++ client/src/components/Leftbar/Leftbar.tsx
@@ -1,7 +1,22 @@
 import "./leftbar.scss";
 
 const Leftbar = (): JSX.Element => {
-  return <div className="leftbar">Leftbar</div>;
+  return (
+    <div className="leftbar">
+      <div className="container">
+        <div className="menu">
+          <div className="user">
+            <img src="" alt="" />
+            <span>Retro Techie</span>
+          </div>
+          <div className="item">
+            <img src="" alt="" />
+            <span>Friends</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Leftbar;
