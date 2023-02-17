4703e684ceb4fcbc38bc434d39a8695b8c1d938a
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-02-16 20:28:40 -0800

    feat(asset): add assets to `Leftbar`
    To use non-code assets with TypeScript, defer the type for these
    imports. This requires a /custom.d.ts which signifies custom definitions
    for TypeScript in the project.
    
    Issue #20
    Reference:
    - [TypeScript Guides](https://webpack.js.org/guides/typescript/)

--- client/src/components/Leftbar/Leftbar.tsx
+++ client/src/components/Leftbar/Leftbar.tsx
@@ -1,4 +1,17 @@
 import "./leftbar.scss";
+import Friends from "../../assets/1.png";
+import Groups from "../../assets/2.png";
+import Market from "../../assets/3.png";
+import Watch from "../../assets/4.png";
+import Memories from "../../assets/5.png";
+import Events from "../../assets/6.png";
+import Gaming from "../../assets/7.png";
+import Gallery from "../../assets/8.png";
+import Videos from "../../assets/9.png";
+import Messages from "../../assets/10.png";
+import Tutorial from "../../assets/11.png";
+import Courses from "../../assets/12.png";
+import Fund from "../../assets/13.png";
 
 const Leftbar = (): JSX.Element => {
   return (
@@ -10,7 +23,7 @@ const Leftbar = (): JSX.Element => {
             <span>Retro Techie</span>
           </div>
           <div className="item">
-            <img src="" alt="" />
+            <img src={Friends} alt="" />
             <span>Friends</span>
           </div>
         </div>
