cb9995fb356592ce2670dc06a156455127adba54
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-02-18 11:45:10 -0800

    feat(home): implement `flex` to `Outlet` section
    This separates all components on `Home` page
    - `Navbar`
    - `Leftbar`
    - `Rightbar`
    - `Outlet` (`Home` content)
    
    Issue #25

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,9 @@ function App(): JSX.Element {
         <Navbar />
         <div style={{ display: "flex" }}>
           <Leftbar />
-          <Outlet />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
           <Rightbar />
         </div>
       </div>
