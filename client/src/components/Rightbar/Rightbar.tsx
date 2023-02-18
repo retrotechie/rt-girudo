274b4a2b3ab49c3cb07e2b7124dd52b49cc2c65f
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-02-17 16:52:25 -0800

    feat(rightbar): design `Rightbar` component
    Create `Suggestions` section in `Rightbar`.
    
    Issue #21
    Reference:
    - [Box Shadow](https://www.cssmatic.com/box-shadow)

--- client/src/components/Rightbar/Rightbar.tsx
+++ client/src/components/Rightbar/Rightbar.tsx
@@ -1,7 +1,28 @@
 import "./rightbar.scss";
 
 const Rightbar = (): JSX.Element => {
-  return <div className="rightbar">Rightbar</div>;
+  return (
+    <div className="rightbar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions for you</span>
+          <div className="user">
+            <div className="user-info">
+              <img
+                src="https://images.pexels.com/photos/1227511/pexels-photo-1227511.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+              />
+              <span>Elon Musk</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;
