7b8508eb4da0805f6a1f1dcb0fb474a987e7607b
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-02-28 10:58:04 -0800

    feat(auth): create `AuthContext`
    Authenticate users with implementation of `AuthContext`.
    
    `Login` varifies users by calling `login()` from `AuthContext`, but for
    now, we only set temporary user info for testing.
    
    `Leftbar` & `Navbar` update user name and profile picture based on
    logged in user information.
    
    Fix typo `img` element in /leftbar.scss
    
    Issue #28

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { AuthContextProvider } from "./context/Auth";
 import { DarkModeContextProvider } from "./context/DarkMode";
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <DarkModeContextProvider>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </DarkModeContextProvider>
   </React.StrictMode>
 );
