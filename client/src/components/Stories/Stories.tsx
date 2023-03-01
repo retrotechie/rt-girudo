71c78d77677d990c037b0b87122f9aec513bb176
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-03-01 10:14:29 -0800

    feat(stories): design `Stories` component
    Add temporary `story` for testing.
    
    Add `Stories` component to `Home` page.
    
    Issue #30

--- client/src/components/Stories/Stories.tsx
+++ client/src/components/Stories/Stories.tsx
@@ -1,7 +1,59 @@
 import "./stories.scss";
 
+interface Story {
+  id: number;
+  name: string;
+  content: string;
+}
+
 const Stories = (): JSX.Element => {
-  return <div className="stories">Stories</div>;
+  const stories: Story[] = [
+    // Temporary
+    {
+      id: 1,
+      name: "Story 1",
+      content:
+        "https://images.pexels.com/photos/1227511/pexels-photo-1227511.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 2,
+      name: "Story 2",
+      content:
+        "https://images.pexels.com/photos/1227511/pexels-photo-1227511.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 3,
+      name: "Story 3",
+      content:
+        "https://images.pexels.com/photos/1227511/pexels-photo-1227511.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 4,
+      name: "Story 4",
+      content:
+        "https://images.pexels.com/photos/1227511/pexels-photo-1227511.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+    {
+      id: 5,
+      name: "Story 5",
+      content:
+        "https://images.pexels.com/photos/1227511/pexels-photo-1227511.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    },
+  ];
+
+  return (
+    <div className="stories">
+      {
+        // Use `map` to iterate over `stories` array
+        stories.map((story) => (
+          <div className="story">
+            <img src={story.content} alt="" />
+            <span>{story.name}</span>
+          </div>
+        ))
+      }
+    </div>
+  );
 };
 
 export default Stories;
