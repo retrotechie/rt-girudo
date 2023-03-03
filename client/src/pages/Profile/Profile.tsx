432e5a429395abf7ec2e082469d451836a94a0e3
Retro Techie <retrotechie@proton.me>
2023-03-02 23:34:42 -0800

    feat(profile): design `Profile` page
    Issue #40

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -1,7 +1,83 @@
+import FacebookTwoToneIcon from "@mui/icons-material/FacebookTwoTone";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import PlaceIcon from "@mui/icons-material/Place";
+import LanguageIcon from "@mui/icons-material/Language";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
 import "./profile.scss";
 
 const Profile = (): JSX.Element => {
-  return <div className="profile">Profile</div>;
+  return (
+    <div className="profile">
+      <div className="images">
+        <img
+          src="https://images.pexels.com/photos/13440765/pexels-photo-13440765.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          alt=""
+          className="coverPic"
+        />
+
+        <img
+          src="https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
+          alt=""
+          className="profilePic"
+        />
+      </div>
+
+      <div className="profile-container">
+        <div className="user-info">
+          <div className="left">
+            <a href="http://facebook.com">
+              <FacebookTwoToneIcon fontSize="medium" />
+            </a>
+
+            <a href="http://facebook.com">
+              <InstagramIcon fontSize="medium" />
+            </a>
+
+            <a href="http://facebook.com">
+              <TwitterIcon fontSize="medium" />
+            </a>
+
+            <a href="http://facebook.com">
+              <LinkedInIcon fontSize="medium" />
+            </a>
+
+            <a href="http://facebook.com">
+              <PinterestIcon fontSize="medium" />
+            </a>
+          </div>
+
+          <div className="center">
+            <span>User Name</span>
+
+            <div className="info">
+              <div className="item">
+                <PlaceIcon />
+                <span>USA</span>
+              </div>
+
+              <div className="item">
+                <LanguageIcon />
+                <span>retrotechie.dev</span>
+              </div>
+            </div>
+
+            <button>Follow</button>
+          </div>
+
+          <div className="right">
+            <EmailOutlinedIcon />
+
+            <MoreVertIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
