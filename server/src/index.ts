351c28d14b0fd15d895c39ad8d99ffa8a7204eb3
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-03-07 13:38:48 -0800

    feat(route): create server routes
    Create routes for `users`, `posts`, `comments`, `likes` to define the
    endpoints on server side.
    
    Issue #53

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,15 @@
 import express from "express";
-
 const app = express();
+
+import userRoutes from "./routes/users";
+import postRoutes from "./routes/posts";
+import commentRoutes from "./routes/comments";
+import likeRoutes from "./routes/likes";
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
 
 app.listen(8800, () => {
   console.log("API working");
