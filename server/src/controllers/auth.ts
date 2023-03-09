05bc9c592114a59ef8170b0699f11a68212af1c3
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-03-09 15:24:19 -0800

    feat(auth): create server `auth`
    Create `auth` router and controller.
    
    Issue #56

---
+++ server/src/controllers/auth.ts
@@ -0,0 +1,7 @@
+export const register = (req, res) => {
+    // Check if user exists
+    // Create a new user
+    // Hash the password
+};
+export const login = (req, res) => {};
+export const logout = (req, res) => {};
