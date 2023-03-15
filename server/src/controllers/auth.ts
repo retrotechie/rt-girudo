48b61d28b4cf0babc0c5308a9bcc6b789328e6d0
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-03-15 14:47:33 -0700

    feat(auth): create a new user
    Create a new user if provided user info not exists in database.
    
    Issue #56

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -3,19 +3,33 @@ import bcrypt from "bcryptjs";
 
 export const register = (req, res) => {
   // * Check if user exists
-  const q = "SELECT FROM users WHERE username = ?"; // Use `?` instead of `req.body.username` for security
+  const checkUserQuery = "SELECT FROM `Users` WHERE `username` = ?"; // Use `?` instead of `req.body.username` for security
 
-  db.query(q, [req.body.username], (err, data) => {
+  db.query(checkUserQuery, [req.body.username], (err, data) => {
     if (err) return res.status(500).json(err);
 
-    if (data.length) return res.status(409).json;
-    ("User exists!");
-  });
+    if (data.length) return res.status(409).json("User exists!");
 
-  // * Create a new user
-  // Hash the password
-  const salt = bcrypt.genSaltSync(10);
-  const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+    // * Create a new user
+    // Hash the password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+
+    const createUserQuery =
+      "INSERT INTO `Users` (`username`, `email`, `password`, `name`) VALUE (?)";
+
+    const values = [
+      req.body.username,
+      req.body.email,
+      hashedPassword,
+      req.body.name,
+    ];
+    db.query(createUserQuery, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("User has been created");
+    });
+  });
 };
+
 export const login = (req, res) => {};
 export const logout = (req, res) => {};
