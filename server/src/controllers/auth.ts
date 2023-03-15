e1123220954ee3d72ba4efb5589623db539eb522
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-03-15 13:43:44 -0700

    feat(auth): check if user exists
    Create a query to check if user exists in MySQL database and return
    appropriate `status` code for the checking result.
    
    Create `express.json()` middleware for parsing JSON request bodies.
    
    Issue #56
    Reference:
    - [Using middleware](https://expressjs.com/en/guide/using-middleware.html)
    See also:
    - [Writing middleware](https://expressjs.com/en/guide/writing-middleware.html)
    - [Building Middlware for Node.js](https://www.turing.com/kb/building-middleware-for-node-js)

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,7 +1,18 @@
+import { db } from "../../db/connect";
+
 export const register = (req, res) => {
-    // Check if user exists
-    // Create a new user
-    // Hash the password
+  // * Check if user exists
+  const q = "SELECT FROM users WHERE username = ?"; // Use `?` instead of `req.body.username` for security
+
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+
+    if (data.length) return res.status(409).json;
+    ("User exists!");
+  });
+
+  // Create a new user
+  // Hash the password
 };
 export const login = (req, res) => {};
 export const logout = (req, res) => {};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,16 @@
 import express from "express";
 const app = express();
 
+import authRoutes from "./routes/auth";
 import userRoutes from "./routes/users";
 import postRoutes from "./routes/posts";
 import commentRoutes from "./routes/comments";
 import likeRoutes from "./routes/likes";
 
+// * Middlewares
+app.use(express.json);
+
+app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
