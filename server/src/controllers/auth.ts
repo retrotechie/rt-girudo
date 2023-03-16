4bd12fbb868af47dec54e979887de346380cf100
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-03-16 11:02:29 -0700

    feat(auth): perform user `login`
    Look up `username` in database and `checkPassword` to verify provided
    info before granting access.
    
    Issue #56

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -4,8 +4,10 @@ import { Request, Response } from "express";
 
 export const register = (req: Request, res: Response): void => {
   // * Check if user exists
-  const checkUserQuery = "SELECT * FROM `Users` WHERE `username` = ?"; // Use `?` instead of `req.body.username` for security
+  // Use `?` instead of `req.body.username` for security
+  const checkUserQuery = "SELECT * FROM `Users` WHERE `username` = ?";
 
+  // FIXME: Change `any` to a specific type
   db.query(checkUserQuery, [req.body.username], (err: any, data: any) => {
     if (err) return res.status(500).json(err);
 
@@ -34,7 +36,21 @@ export const register = (req: Request, res: Response): void => {
 };
 
 export const login = (req: Request, res: Response): void => {
-  console.log("Login");
+  const checkUserQuery = "SELECT * FROM `Users` WHERE `username` = ?";
+
+  db.query(checkUserQuery, [req.body.username], (err: any, data: any) => {
+    if (err) return res.status(500).json(err);
+
+    if (data.length === 0) return res.status(404).json("User not found!");
+
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password // `data[0]` bc `SELECT *` returns an array
+    );
+
+    if (!checkPassword)
+      return res.status(400).json("Wrong password or username!");
+  });
 };
 export const logout = (req: Request, res: Response): void => {
   console.log("Logout");
