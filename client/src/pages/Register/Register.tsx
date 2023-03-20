8b6378e9d3c6da071f9ec892354abca1bbd018bd
Retro Techie <107520616+retrotechie@users.noreply.github.com>
2023-03-20 00:14:25 -0700

    feat(register): get user inputs
    Create a stateful component `[inputs, setInputs]` (a `state` variable)
    that can handle changes to multiple input fields in a form by keeping
    track of their values in state & updating that state whenever the user
    types into one of the fields.
    
    The `[inputs, setInputs]` state variable contains:
    - `inputs` variable is the current state value, which is initially set
      to `{username: "", email: "", ...}`.
    - `setInputs` variable is a function that can be used to update the
      state value.
    
    By using state in this way, React can keep track of the values of the
    input fields in the form, and re-render the component whenever the state
    changes, ensuring that the component always reflects the current state
    of the application.
    
    Issue #64
    Reference:
    - [React.js State](https://www.simplilearn.com/tutorials/reactjs-tutorial/reactjs-state)

--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -1,7 +1,27 @@
 import "./register.scss";
 import { Link } from "react-router-dom";
+import { ChangeEvent, useState } from "react";
 
 const Register = (): JSX.Element => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+    name: "",
+  });
+
+  // `handleChange` is called every time an input field's value changes
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    // `setInputs` is called to update the state of the component. It takes the
+    // previous state as its argument & returns a new state object that is a
+    // copy of the previous state object `...prev`, but with the value of the
+    // input field that triggered the change event updated to the new value
+    // `[e.target.name]:e.target.value`
+    setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  console.log(inputs);
+
   return (
     <div className="register">
       <div className="card">
@@ -21,10 +41,30 @@ const Register = (): JSX.Element => {
         <div className="right">
           <h1>Register</h1>
           <form>
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="text" placeholder="Name" />
+            <input
+              type="text"
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              onChange={handleChange}
+            />
             <button>Register</button>
           </form>
         </div>
